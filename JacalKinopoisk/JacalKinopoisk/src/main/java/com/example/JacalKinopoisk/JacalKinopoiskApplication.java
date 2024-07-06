88399244e9f471955d1cec87e8197e844f0c30a6
Vuulgrim <98886483+Vuulgrim@users.noreply.github.com>
2024-07-06 16:06:22 +0300

    Add files via upload
    Start of a project

---
+++ JacalKinopoisk/JacalKinopoisk/src/main/java/com/example/JacalKinopoisk/JacalKinopoiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.JacalKinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacalKinopoiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacalKinopoiskApplication.class, args);
+	}
+
+}
---
+++ JacalKinopoisk/JacalKinopoisk/src/test/java/com/example/JacalKinopoisk/JacalKinopoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JacalKinopoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacalKinopoiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
